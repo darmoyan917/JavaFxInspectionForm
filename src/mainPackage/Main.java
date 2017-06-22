052a342c4561c3bb9b778b23fa80dc45a0925692
Karo <darmoyan916@gmail.com>
2017-06-22 15:29:37 -0700

    Basic info added to panels.

--- src/mainPackage/Main.java
+++ src/mainPackage/Main.java
@@ -6,19 +6,20 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("mainLayout.fxml"));
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("mainLayout.fxml"));
+        //FXMLLoader loader = new FXMLLoader(getClass().getResource("mainLayout.fxml"));
 
         primaryStage.setTitle("Inspection Form");
         primaryStage.getIcons().add(new Image("/images/icon.png"));
         primaryStage.setScene(new Scene(root, 1200, 900));
         primaryStage.show();
-        mainController controller1 = loader.getController();
+        //mainController controller1 = loader.getController();
         //controller1.setStage(primaryStage);
     }
 
